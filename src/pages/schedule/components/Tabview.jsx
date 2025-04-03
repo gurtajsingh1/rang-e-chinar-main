b0a89fef084454296a91c28f57496c0abc1ac259
ankrit <jarngalankrit@gmail.com>
2025-04-04 02:16:46 +0530

    change

--- src/pages/schedule/components/Tabview.jsx
+++ src/pages/schedule/components/Tabview.jsx
@@ -6,28 +6,32 @@ const Tabview = ({ tab1, tab2 }) => {
   return (
     <Tabs>
       {/* Tab Buttons */}
-      <TabList className="flex flex-wrap justify-end mr-6 md:mr-18 gap-6 my-6 font-kodeMono">
+      <TabList className="flex flex-wrap justify-end mr-6 md:mr-18 gap-6 my-6 font-quicksand">
+        {/* Tab 1 */}
         <Tab
           className="relative px-16 py-3 text-lg font-bold border-2 rounded-lg transition-all duration-300 cursor-pointer
-                     bg-[#F5E1C6] text-[#D97706] border-[#D97706]"
-          selectedClassName="!bg-[#D97706] !text-[#F5E1C6] !border-[#D97706] 
-                             !shadow-[0_0_15px_rgba(217,119,6,0.4)] 
-                             !scale-x-110"
+          bg-[#F5E1C6] text-[#D97706] border-[#D97706] font-playfair"
+          selectedClassName="!bg-[#D97706] !text-[#F5E1C6] !border-[#D97706]
+          !shadow-[0_0_15px_rgba(217,119,6,0.4)]
+          !scale-x-110"
         >
           {tab1}
         </Tab>
+
+        {/* Tab 2 (Fixed Hover Issue) */}
         <Tab
-          selectedClassName="active-tab"
-          className={
-            "px-14 py-3 text-base font-medium text-gray-800 bg-white border-gray-800 focus:outline-none hover:bg-secondary hover:text-gray hover:border-primary text-center border-2 tab-button cursor-pointer"
-          }
+          className="relative px-14 py-3 text-lg font-bold border-2 rounded-lg transition-all duration-300 cursor-pointer
+          bg-[#F5E1C6] text-[#D97706] border-[#D97706] hover:bg-[#D97706] hover:text-[#F5E1C6] hover:border-[#D97706] font-playfair"
+          selectedClassName="!bg-[#D97706] !text-[#F5E1C6] !border-[#D97706]
+          !shadow-[0_0_15px_rgba(217,119,6,0.4)]
+          !scale-x-110"
         >
           {tab2}
         </Tab>
       </TabList>
 
       {/* Content Box */}
-      <div className="mx-2 my-1 px-4 py-6 pb-12 rounded-lg">
+      <div className="mx-2 my-1 px-4 py-6 pb-12 rounded-lg font-quicksand">
         <TabPanel className="space-y-6">
           {day1Events.map((item, index) => (
             <ScheduleItem
@@ -36,10 +40,11 @@ const Tabview = ({ tab1, tab2 }) => {
               startTime={item["Start Time"]}
               endTime={item["End Time"]}
               venue={item["Venue"]}
-              exploreUrl={`/events/${item.url}`} 
+              exploreUrl={`/events/${item.url}`}
             />
           ))}
         </TabPanel>
+
         <TabPanel className="space-y-6">
           {day2Events.map((item, index) => (
             <ScheduleItem
@@ -48,7 +53,7 @@ const Tabview = ({ tab1, tab2 }) => {
               startTime={item["Start Time"]}
               endTime={item["End Time"]}
               venue={item["Venue"]}
-              exploreUrl={`/events/${item.url}`} 
+              exploreUrl={`/events/${item.url}`}
             />
           ))}
         </TabPanel>
